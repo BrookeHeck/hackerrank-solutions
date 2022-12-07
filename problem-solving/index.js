64f5a6ca90cf1bd8ac00f184a982fa4f248ead8d
Brooke Heck <b19heck@gmail.com>
2022-12-07 03:29:41 -0800

    fix time conversion

--- problem-solving/index.js
+++ problem-solving/index.js
@@ -1,9 +1,11 @@
 const timeConversions = require('./time-conversion');
 
-const time = '12:01:00PM';
+const time = '12:01:00AM';
 const time2 = '07:05:45PM';
 const time3 = '09:00:11AM';
+const time4 = '12:00:00PM';
 
 console.log(timeConversions(time));
 console.log(timeConversions(time2));
 console.log(timeConversions(time3));
+console.log(timeConversions(time4));
--- problem-solving/time-conversion.js
+++ problem-solving/time-conversion.js
@@ -1,15 +1,18 @@
 'use strict';
 
-function timeConversions(time) {
-  const numChars = time.length;
-  const indicator = time.substr(numChars - 2, numChars);
-  if(indicator === 'AM') return time.substr(0, numChars - 2);
-  else {
-    let hour = parseInt(time.substr(0, 2));
-    hour += 12;
-    if(hour === 24) hour = '00';
-    return `${hour}${time.substr(2, numChars - 2)}`;
+function timeConversions(s) {
+  const numChars = s.length;
+  const indicator = s.substr(numChars - 2, numChars );
+  let hour = parseInt(s.substr(0, 2));
+  if(indicator === 'AM') {
+    if(hour === 12) hour = '00';
+    else hour = `0${hour}`;
   }
+  else {
+    hour += 12;
+    if(hour === 24) hour = '12';
+  }
+  return `${hour}${s.substr(2, numChars - 4)}`;
 }
 
 module.exports = timeConversions;

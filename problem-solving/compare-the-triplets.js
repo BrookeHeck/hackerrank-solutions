7f46291e87c34798950830a6372f2ced4a049812
Brooke Heck <b19heck@gmail.com>
2022-12-07 02:29:37 -0800

    add a merge sort

--- problem-solving/compare-the-triplets.js
+++ problem-solving/compare-the-triplets.js
@@ -1,9 +1,11 @@
+'use strict';
+
 function compareTriplets(a, b) {
-    const alice = 0;
-    const bob = 0;
-    for(let i = 0; i < 3; i++) {
-        if(a[i] > b[i]) alice++;
-        if(a[i] < b[i]) bob++; 
-    }
-    return [alice, bob];
+	let alice = 0;
+  let bob = 0;
+  for(let i = 0; i < 3; i++) {
+    if(a[i] > b[i]) alice++;
+    if(a[i] < b[i]) bob++; 
+  }
+  return [alice, bob];
 }
---
+++ problem-solving/index.js
@@ -0,0 +1,3 @@
+const minMaxSum = require('./min-max-sum');
+console.log('hello');
+minMaxSum([1, 5, 3, 2, 4, 5, 0]);

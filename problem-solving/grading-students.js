85cbbe2e2b610410ef62c64a918b71743cb65a9e
Brooke Heck <b19heck@gmail.com>
2022-12-07 08:32:32 -0800

    fix grading students challenge

--- problem-solving/grading-students.js
+++ problem-solving/grading-students.js
@@ -3,14 +3,14 @@
 function gradingStudents(grades) {
   const finalGrades = [];
 
-  for (let i = 1; i < grades.length; i++) {
+  for (let i = 0; i < grades.length; i++) {
     // for a failing grad
     if (grades[i] < 38) finalGrades.push(grades[i]);
 
     // find rounding up multiple of five
     else {
       let nearestMultiple = grades[i];
-      while (grades[i] % 5 !== 0) {
+      while (nearestMultiple % 5 !== 0) {
         nearestMultiple++;
       }
       if ((nearestMultiple - grades[i]) < 3) {
@@ -19,6 +19,7 @@ function gradingStudents(grades) {
       finalGrades.push(grades[i]);
     }
   }
+  return finalGrades;
 }
 
 module.exports = gradingStudents;
